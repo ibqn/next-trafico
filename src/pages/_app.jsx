05c5f67a4114e521ba6d7d6fb158c7968fbfa598
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-11-13 22:31:45 +0100

    adjust font weights

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,11 @@
 import "styles/globals.css"
 
-import "@fontsource/rubik/index.css"
+import "@fontsource/rubik/300.css"
+import "@fontsource/rubik/400.css"
+import "@fontsource/rubik/500.css"
+import "@fontsource/rubik/600.css"
+import "@fontsource/rubik/700.css"
+
 import "@fontsource/dm-serif-display/index.css"
 
 function MyApp({ Component, pageProps }) {

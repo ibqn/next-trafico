9417a6f4688fc0029d876729a824cf618cbc5276
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-11-13 22:20:28 +0100

    style testimonials

--- src/components/slider-card/slider-card.jsx
+++ src/components/slider-card/slider-card.jsx
@@ -4,11 +4,20 @@ export const SliderCard = ({ client }) => {
   const { name, message, image } = client
 
   return (
-    <div>
-      <p>{message}</p>
-      <div>
-        <Image src={image} width="60" height="60" alt="Image of client" />
-        <span>{name}</span>
+    <div className="flex h-[360px] flex-col justify-between rounded-[10px] bg-white px-[50px] pt-[60px] pb-[40px] drop-shadow-primary">
+      <p className="font-light leading-[30px]">{message}</p>
+      <div className="flex items-center justify-center gap-x-5">
+        <div className="aspect-square h-[60px] w-[60px]">
+          <Image
+            src={image}
+            width="60"
+            height="60"
+            className="rounded-full"
+            alt="Image of client"
+          />
+        </div>
+
+        <span className="font-semibold">{name}</span>
       </div>
     </div>
   )
--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -4,14 +4,16 @@ import { testimonialData } from "data"
 export const Testimonials = () => {
   const { clients, preTitle, title } = testimonialData
   return (
-    <section className="min-h-[600px] bg-green-100">
+    <section className="relative min-h-[600px]">
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col lg:flex-row">
           <div className="lg:w-2/5">
             <h3 className="h3">{preTitle}</h3>
             <h2 className="h2 mb-6">{title}</h2>
           </div>
-          <Slider clients={clients} />
+          <div className="lg:absolute lg:right-0 lg:w-3/5">
+            <Slider clients={clients} />
+          </div>
         </div>
       </div>
     </section>
